5d1f1a1f073eb917396881b820bfb85e336ce669
Dope69 <de.vrieling@st.hanze.nl>
2017-06-27 17:01:46 +0200

    Salt minion opstart script

---
+++ Salt-minion.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+#Wordt root
+sudo -i
+
+#Minion naar hosts
+echo "127.0.0.1 minion" >> /etc/hosts
+
+#installeer salt-minion
+apt-get install salt-minion -y
+
+#ipadres van master: 10.5.0.142 in config aanzetten
+sed -i 's/#master: salt/master: 10.5.0.142/g' /etc/salt/minion
+
+#herstarten minion
+service salt-minion restart
+
+
+
+
+
+
+
